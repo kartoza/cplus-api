26d543c12f541721854f10083d4d4a368b18104c
Danang <danangmassandy@gmail.com>
2024-03-26 10:00:40 +0700

    add analysis code; fix python3.11 as base image for pyqgis

---
+++ django_project/cplus/definitions/constants.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Definitions for application constants.
+"""
+
+
+NCS_PATHWAY_SEGMENT = "ncs_pathways"
+NCS_CARBON_SEGMENT = "ncs_carbon"
+PRIORITY_LAYERS_SEGMENT = "priority_layers"
+
+# Naming for outputs sub-folder relative to base directory
+OUTPUTS_SEGMENT = "outputs"
+
+IM_GROUP_LAYER_NAME = "Implementation Model Maps"
+IM_WEIGHTED_GROUP_NAME = "Weighted Implementation Model Maps"
+NCS_PATHWAYS_GROUP_LAYER_NAME = "NCS Pathways Maps"
+
+# Attribute names
+CARBON_COEFFICIENT_ATTRIBUTE = "carbon_coefficient"
+CARBON_PATHS_ATTRIBUTE = "carbon_paths"
+COLOR_RAMP_PROPERTIES_ATTRIBUTE = "color_ramp"
+COLOR_RAMP_TYPE_ATTRIBUTE = "ramp_type"
+DESCRIPTION_ATTRIBUTE = "description"
+IM_LAYER_STYLE_ATTRIBUTE = "model_layer"
+IM_SCENARIO_STYLE_ATTRIBUTE = "scenario_layer"
+LAYER_TYPE_ATTRIBUTE = "layer_type"
+NAME_ATTRIBUTE = "name"
+PATH_ATTRIBUTE = "path"
+PATHWAYS_ATTRIBUTE = "pathways"
+PIXEL_VALUE_ATTRIBUTE = "style_pixel_value"
+STYLE_ATTRIBUTE = "style"
+USER_DEFINED_ATTRIBUTE = "user_defined"
+UUID_ATTRIBUTE = "uuid"
+
+MODEL_IDENTIFIER_PROPERTY = "model_identifier"
