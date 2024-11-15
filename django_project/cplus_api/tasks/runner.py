0086259b5a125b679cb25b82bb3df02ce593efec
Danang <danangmassandy@gmail.com>
2024-11-15 06:29:14 +0000

    Refactor to use cplus core  package (#69)
    * use core package for ScenarioAnalysisTask
    
    * fix lint and update docstring
    
    * remove log
    
    * fix activities in scenario
    
    * Update CPLUS core version
    
    ---------
    
    Co-authored-by: Zulfikar Akbar Muzakki <zakki@kartoza.com>

--- django_project/cplus_api/tasks/runner.py
+++ django_project/cplus_api/tasks/runner.py
@@ -3,32 +3,39 @@
 from celery import shared_task
 import logging
 import time
-import json
 from django.conf import settings
-from core.settings.utils import UUIDEncoder
 from cplus_api.models.scenario import ScenarioTask
 
 logger = logging.getLogger(__name__)
 
 
 def create_scenario_task_runner(scenario_task: ScenarioTask):
+    """Create and prepare worker task.
+
+    :param scenario_task: scenario task object
+    :type scenario_task: ScenarioTask
+    :return: worker task that is ready to be run
+    :rtype: WorkerScenarioAnalysisTask
+    """
     # below imports require PyQGIS to be initialised
     from cplus_api.utils.worker_analysis import (
-        TaskConfig, WorkerScenarioAnalysisTask
+        APITaskConfig, WorkerScenarioAnalysisTask
     )
 
-    task_config = TaskConfig.from_dict(scenario_task.detail)
+    task_config = APITaskConfig.from_dict(scenario_task.detail)
     analysis_task = WorkerScenarioAnalysisTask(task_config, scenario_task)
-    logger.info('Started prepare_run')
     analysis_task.prepare_run()
-    logger.info('Finished prepare_run')
-    logger.info(
-        json.dumps(analysis_task.task_config.to_dict(), cls=UUIDEncoder))
     return analysis_task
 
 
 @shared_task(name="run_scenario_analysis_task")
-def run_scenario_analysis_task(scenario_task_id):  # pragma: no cover
+def run_scenario_analysis_task(scenario_task_id):
+    """Run the scenario analysis.
+
+    :param scenario_task_id: scenario task object id
+    :type scenario_task_id: int
+    """
+    # Fetch ScenarioTask Object
     scenario_task = ScenarioTask.objects.get(id=scenario_task_id)
     scenario_task.task_on_started()
     scenario_task.code_version = (
@@ -37,6 +44,8 @@ def run_scenario_analysis_task(scenario_task_id):  # pragma: no cover
     scenario_task.save(update_fields=['code_version'])
     logger.info(
         f'Triggered run_scenario_analysis_task {str(scenario_task.uuid)}')
+
+    # initialize QGIS
     from qgis.core import QgsApplication
     # Supply path to qgis install location
     QgsApplication.setPrefixPath("/usr/bin/qgis", True)
@@ -45,19 +54,22 @@ def run_scenario_analysis_task(scenario_task_id):  # pragma: no cover
     qgs = QgsApplication([], False)
     # Load providers
     qgs.initQgis()
+
     # init processing plugins
     import processing  # noqa
     from processing.core.Processing import Processing
     Processing.initialize()
 
+    # Run scenario task
     analysis_task = create_scenario_task_runner(scenario_task)
-
     start_time = time.time()
     analysis_task.run()
     logger.info(f'execution time: {time.time() - start_time} seconds')
+
     # call finished() to upload layer outputs
     analysis_task.finished(True)
+
     # use qgs.exit() if worker can be reused to execute another task
     qgs.exit()
-    # exitQgis causing worker lost
+    # NOTE: exitQgis causing worker lost
     # qgs.exitQgis()
